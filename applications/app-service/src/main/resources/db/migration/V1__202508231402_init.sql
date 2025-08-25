2693af4f14b79e379da1d960be40e59e74a9393e
Luis Plata <www.luisplata@gmail.com>
2025-08-24 21:39:24 -0500

    Update SQL to diagram

--- applications/app-service/src/main/resources/db/migration/V1__202508231402_init.sql
+++ applications/app-service/src/main/resources/db/migration/V1__202508231402_init.sql
@@ -1,10 +1,20 @@
--- V1__create_usuario_table.sql
+CREATE TABLE rol (
+    uniqueid BIGSERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(255)
+);
+
 CREATE TABLE usuario (
-    id BIGSERIAL PRIMARY KEY,
+    id_usuario BIGSERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT NULL,
-    email VARCHAR(150) NOT NULL,
-    fecha_nacimiento DATE NOT NULL,
-    direccion VARCHAR(200),
-    salario_base BIGINT NOT NULL
+    email VARCHAR(150) UNIQUE NOT NULL,
+    documento_identidad VARCHAR(50) UNIQUE NOT NULL,
+    telefono VARCHAR(20),
+    id_rol BIGINT,
+    salario_base NUMERIC(10,2),
+    CONSTRAINT fk_usuario_rol FOREIGN KEY (id_rol)
+        REFERENCES rol(uniqueid)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL
 );
