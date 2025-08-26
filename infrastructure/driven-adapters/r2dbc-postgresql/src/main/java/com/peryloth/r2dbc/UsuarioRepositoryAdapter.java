3e338b2aed8d67aeff59f441aed8c61f3c03ef05
Luis Plata <www.luisplata@gmail.com>
2025-08-26 08:31:11 -0500

    delete SQS implementation

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/com/peryloth/r2dbc/UsuarioRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/com/peryloth/r2dbc/UsuarioRepositoryAdapter.java
@@ -9,6 +9,7 @@ import org.reactivecommons.utils.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 
 import java.math.BigInteger;
@@ -23,11 +24,12 @@ public class UsuarioRepositoryAdapter extends ReactiveAdapterOperations<
 
     private static final Logger log = LoggerFactory.getLogger(UsuarioRepositoryAdapter.class);
 
-    public UsuarioRepositoryAdapter(UsuarioReactiveRepository repository, RolReactiveRepository rolRepository, ObjectMapper mapper) {
+    public UsuarioRepositoryAdapter(UsuarioReactiveRepository repository, ObjectMapper mapper) {
         super(repository, mapper, d -> mapper.map(d, Usuario.class));
     }
 
     @Override
+    @Transactional
     public Mono<Usuario> saveUsuario(Usuario usuario) {
         UsuarioEntity entity = mapper.map(usuario, UsuarioEntity.class);
         BigInteger rolId = usuario != null && usuario.getRol() != null ? usuario.getRol().getUniqueId() : null;
